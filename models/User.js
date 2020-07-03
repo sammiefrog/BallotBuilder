b051866c614f99c75521508f4d0b4c75935ad5cd
sammiefrog <sammiefrog@live.com>
2020-07-02 19:55:28 -0500

    wrote a user model

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        min: 6,
+        required: true
+    },
+    password: {
+        type: String,
+        min: 6,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
