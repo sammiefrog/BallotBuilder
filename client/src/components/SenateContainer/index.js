2d259694e21e3821ba615836e057ffb88d676904
Josh Everett <josheverett93@gmail.com>
2020-07-11 10:52:18 -0500

    added outline for 3 election containers

---
+++ client/src/components/SenateContainer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import GridList from '@material-ui/core/GridList';
+
+export default function SenateContainer() {
+    return (
+        <Container>
+            <GridList>
+
+            </GridList>
+        </Container>
+    );
+}
