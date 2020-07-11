b34e3a058411282ba912c5904bd5e80961aa6c11
Josh Everett <josheverett93@gmail.com>
2020-07-11 08:56:14 -0500

    added centered tabs to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,8 @@
 import React, { useContext, useEffect } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Paper from "@material-ui/core/Paper";
 
 // where election info goes
 // put centered tab on home page
@@ -8,12 +12,33 @@ import React, { useContext, useEffect } from 'react';
 // card component - map when I bring into election data
 // around each card is a box and around the box is a grid list
 // keep box in card component and grid list in container component
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
+
 const Home = () => {
-    return ( 
+    const classes = useStyles();
+    return (
         <div>
             <h1>Home Page</h1>
+            <Paper className={classes.root}>
+                <Tabs
+                    // value={value}
+                    // onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                >
+                    <Tab label="Presidential Election" />
+                    <Tab label="Senate Election" />
+                    <Tab label="House Election" />
+                </Tabs>
+            </Paper>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
