9a330bf6d4a7142f320369e054d4a0dc944df9e3
sammiefrog <sammiefrog@live.com>
2020-07-21 21:09:22 -0500

    tentative color theme

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,52 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { UserContext, UserContextProvider } from "./context/contexts/UserContext";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#1769aa",
+            dark: "#2c387e"
+        },
+        secondary: {
+            main: "#ff3d00"
+        }
+    }
+});
 
 function App() {
 
 
     return (
-        <UserContextProvider>
-            <UserContext.Consumer>
-                {({ user }) => {
-                    return (
-                        <Router>
-                            <div>
-                                <AppBarNav />
-                                <Switch>
-                                    <Route exact path={["/", "/home"]} component={Home} />
-                                    {user.loggedIn && (
-                                        <Route exact path="/ballot" component={Ballot} />
-                                    )}
-                                    {!user.loggedIn && (
-                                        <Route exact path="/ballot" component={Login} />
-                                    )}
-                                    <Route exact path="/login" component={Login} />
-                                    <Route exact path="/register" component={Register} />
-                                    <Route exact path="/about" component={About} />
-                                </Switch>
-                            </div>
-                        </Router>
-                    );
-                }}
-            </UserContext.Consumer>
-        </UserContextProvider>
+        <ThemeProvider theme={theme}>
+            <UserContextProvider>
+                <UserContext.Consumer>
+                    {({ user }) => {
+                        return (
+                            <Router>
+                                <div>
+                                    <AppBarNav />
+                                    <Switch>
+                                        <Route exact path={["/", "/home"]} component={Home} />
+                                        {user.loggedIn && (
+                                            <Route exact path="/ballot" component={Ballot} />
+                                        )}
+                                        {!user.loggedIn && (
+                                            <Route exact path="/ballot" component={Login} />
+                                        )}
+                                        <Route exact path="/login" component={Login} />
+                                        <Route exact path="/register" component={Register} />
+                                        <Route exact path="/about" component={About} />
+                                    </Switch>
+                                </div>
+                            </Router>
+                        );
+                    }}
+                </UserContext.Consumer>
+            </UserContextProvider>
+        </ThemeProvider>
     );
 }
 
