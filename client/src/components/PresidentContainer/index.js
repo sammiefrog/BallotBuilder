71d8778755a219721776271a8d428f811b23c207
Josh Everett <josheverett93@gmail.com>
2020-07-28 17:59:52 -0500

    added loading indicator to home page

--- client/src/components/PresidentContainer/index.js
+++ client/src/components/PresidentContainer/index.js
@@ -7,6 +7,7 @@ import API from "../../utils/API";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { UserContext } from "../../context/contexts/UserContext";
+import { trackPromise } from 'react-promise-tracker';
 
 const useStyles = makeStyles({
     root: {
@@ -30,11 +31,12 @@ export default function PresidentContainer() {
     useEffect(() => {
         getPresident();
     }, []);
-
+    
     const getPresident = async () => {
         try {
-            const response = await API.getPresident();
-            const cleanData = response.data.candidate.map(person => ({
+            const response = await trackPromise(API.getPresident());
+            const cleanData = response.data.candidate.map
+            (person => ({
                 ...person,
                 coreValues:
                     "http://votesmart.org/issue_rating_category.php?can_id=" + person.candidateId
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
+
+const LoadingIndicator = props => {
+    const { promiseInProgress } = usePromiseTracker();
+    return (
+        (promiseInProgress === true) ?
+        <div
+            style={{
+              width: "100%",
+              height: "100",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Loader type="ThreeDots" color="#ff3d00" height="100" width="100" />
+          </div>
+        : null
+     );  
+    }
 
 // Rendering application to public html file via root
 ReactDOM.render(
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>,
+    <React.StrictMode>
+    <App />
+    <LoadingIndicator/>
+    </React.StrictMode>,
     document.getElementById("root")
 );
 
