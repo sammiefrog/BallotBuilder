31c6134f0655611f798bb086615cf354b0b5d309
sammiefrog <sammiefrog@live.com>
2020-09-21 20:52:06 -0500

    updating server page

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ require("./routes/userRoutes")(app);
 // Send every other request to the React app
 // Define any API routes before this runs
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Connect to the Mongo DB, handle depreciation warnings
 mongoose
